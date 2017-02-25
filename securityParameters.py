008b085f23092ec027867b1fc2eb58635e831b31
Kevin Häni <kevin.haeni@gmail.com>
2017-02-25 23:36:14 +0100

    comments

--- securityParameters.py
+++ securityParameters.py
@@ -1,8 +1,16 @@
 import gmpy2
 from gmpy2 import mpz
 
-# python doesn't have a const keyword. We are using a class with an intercepting setter to create a const class
-class ConstSecurityParams(object):
+class SecurityParams(object):
+    """
+    This class holds all public security parameters used by many algorithms
+    For every security level, a separate object should exist holding the corresponding values.
+
+    For the purpose of easier unit testing, the parameters for securityLevel0 can then be injected as an ptional parameter into the algorithms
+
+    TODO: Should be made const (intercepting setter) as the values do not change
+    """
+
     p = q = k = g = h = 0
 
     def __init__(self, p, q, k, g, h):
@@ -13,8 +21,7 @@ class ConstSecurityParams(object):
         self.h = h
 
 
-SECURITY_LEVEL_0 = ConstSecurityParams(563, 281, 2, 4, 9)
-SECURITY_LEVEL_1 = ConstSecurityParams(423432, 234234, 23432, 343, 43243)
+SECURITY_LEVEL_0 = SecurityParams(563, 281, 2, 4, 9)
+SECURITY_LEVEL_1 = SecurityParams(423432, 234234, 23432, 343, 43243)
 #....
-
 SECURITY_LEVEL_DEFAULT = SECURITY_LEVEL_0
