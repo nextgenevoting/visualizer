150e60da3ee65c07355db87491448517f6d2768f
Yannick Denzer <denzy1@bfh.ch>
2017-03-11 15:12:12 +0100

    Initial commit of MergeByteArrays

---
+++ MergeByteArrays.py
@@ -0,0 +1,39 @@
+import unittest
+from MergeBytes import MergeBytes
+
+def MergeByteArrays(B1, B2):
+    """
+
+    Algorithm 4.1: Merges the bytes of the two input byte arrays B1 and B2 pairwise.
+    If one byte array is longer than the other, the additional bytes are attached to
+    the result.
+
+    @type  B1: bytearray
+    @param B1: bytearray 1
+
+    @type  B2: bytearray
+    @param B2: bytearray 2
+
+    @rtype:    bytearray
+    @return:   B1 and B2 merged
+    """
+
+    m = min(len(B1), len(B2))
+    B = bytearray()
+
+    for i in range(0, m):
+        B = B + MergeBytes(B1[i], B2[i])
+
+    B = B + B1[m:] + B2[m:]
+
+    return B
+
+class MergeByteArraysTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(MergeByteArrays(bytearray(b'\x00'), bytearray(b'\x00')), bytearray(b'\x00\x00'))
+        self.assertEqual(MergeByteArrays(bytearray(b'\xFF'), bytearray(b'\xFF')), bytearray(b'\xFF\xFF'))
+        self.assertEqual(MergeByteArrays(bytearray(b'\xFF'), bytearray(b'\x00')), bytearray(b'\xAA\xAA'))
+        self.assertEqual(MergeByteArrays(bytearray(b'\x00'), bytearray(b'\xFF')), bytearray(b'\x55\x55'))
+
+if __name__ == '__main__':
+    unittest.main()
