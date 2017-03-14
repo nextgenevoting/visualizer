b2f4a4de4236ccc51849538389554bb7bc72b9ee
Kevin Häni <kevin.haeni@gmail.com>
2017-03-14 09:26:12 +0100

    GetVotingPage: Initial import

---
+++ VotingClient.py
@@ -0,0 +1,12 @@
+class VotingClient(object):
+    """
+    The VotingClient class represents a voting client participating in the protocols (for example prot 6.5)
+    """
+
+
+    def __init__(self, name):
+        self.name = name
+
+    def castVote(self):
+        pass    # TODO (page 50)
+
---
+++ VotingClient/GetVotingPage.py
@@ -0,0 +1,56 @@
+import unittest
+import os, sys
+from gmpy2 import mpz
+import gmpy2
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from Utils.Utils            import AssertMpz
+from Crypto.SecurityParams  import secparams_default
+from Utils.Random          import randomMpz
+
+def GetVotingPage(i, c, n, k):
+    """
+    Algorithm 7.17: Computes a string P in A_UCS^*, which represents the voting page displayed
+    to voter. Specifying the details of presenting the information on the voting page is beyond
+    the scope of this document.
+
+    Args:
+        i (int):    Voter index i in N
+        c (list):   Candidate descriptions c = (C_1, ..., C_n), c_i in A_UCS^*
+        n (list):   Number of candidates n =(n_1, ..., n_t), n_j >= 2
+        k (list):   Number of selections k = (k_1, ..., k_t), 0 <= k_j < n_j
+
+    Returns:
+        string:     String P in A_UCS^* displayed to the voter
+    """
+    electionString = ""
+    candidatePointer = 0
+    for j in range(len(k)):
+        electionString += "# Election {}\n".format(j)
+        for l in range(0,n[j]):
+            electionString += "Candidate: {}\n".format(c[candidatePointer])
+            candidatePointer += 1
+        electionString += "You can make {} selection(s) for this particular election\n\n".format(k[j])
+
+    P = \
+"""
+Voting page for voter {i}:
+
+Simultaneous elections:
+
+{elections}
+
+""".format(
+        i=i,
+        elections=electionString
+    )
+    return P
+
+
+class GetVotingPageTest(unittest.TestCase):
+    def testGetVotingPage(self):
+        print(GetVotingPage(1,["Clinton", "Trump", "Yes", "No"],[2,2],[1,1]))
+
+if __name__ == '__main__':
+    unittest.main()
