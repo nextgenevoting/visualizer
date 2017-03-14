e9206bc2f33c1e8759f05188c41d16dc5c289fb2
Kevin Häni <kevin.haeni@gmail.com>
2017-03-14 09:36:27 +0100

    GetVotingPage: candidate index added

--- VotingClient/GetVotingPage.py
+++ VotingClient/GetVotingPage.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from Utils.Utils            import AssertMpz
 from Crypto.SecurityParams  import secparams_default
-from Utils.Random          import randomMpz
+from Utils.Random           import randomMpz
 
 def GetVotingPage(i, c, n, k):
     """
@@ -25,12 +25,12 @@ def GetVotingPage(i, c, n, k):
         string:     String P in A_UCS^* displayed to the voter
     """
     electionString = ""
-    candidatePointer = 0
+    candidateIndex = 0
     for j in range(len(k)):
         electionString += "# Election {}\n".format(j)
         for l in range(0,n[j]):
-            electionString += "Candidate: {}\n".format(c[candidatePointer])
-            candidatePointer += 1
+            electionString += "Candidate {}: {}\n".format(candidateIndex, c[candidateIndex])
+            candidateIndex += 1
         electionString += "You can make {} selection(s) for this particular election\n\n".format(k[j])
 
     P = \
