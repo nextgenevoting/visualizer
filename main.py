9ae0597bca85c19c2ba2c3f05cbbeb3cbfd6d4ec
Kevin Häni <kevin.haeni@gmail.com>
2017-03-27 11:16:40 +0200

    os path fix

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-from Authority                              import Authority
-from BulletinBoard                          import BulletinBoard
-from Crypto.SecurityParams                  import secparams_default, secparams_l0, secparams_l3
-from VoteClient                             import VoteClient
-from PrintAuthority                         import PrintingAuthority
-
-from Utils.ToString import  ToString
-from Utils.StringToInteger import StringToInteger
-from gmpy2 import mpz
-from ProtocolRunner import ProtocolRunner
+import os
+from ProtocolRunner         import ProtocolRunner
 
 def main():
 
-    level0test = ProtocolRunner("tests\\deterministic_level0.json")
+    level0test = ProtocolRunner(os.path.join("profiles","deterministic_level0.json"))
     level0test.run(False)
 
 
