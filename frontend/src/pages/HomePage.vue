dfa8f6884cdc00480727ed46515983f81f5c3626
Yannick Denzer <denzy1@bfh.ch>
2017-10-29 15:28:39 +0100

    HomePage: remove VueJS logo

--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -1,11 +1,8 @@
 <template>
     <v-container fluid class="pa-0">
-        <div class="blue darken-3 pa-3 text-center">
-            <img src="public/v.png">
-        </div>
         <v-container>
             <h2>Welcome</h2>
-            <p>Welcome to CHVote demonstration</p>
+            <p>Welcome to the CHVote demonstrator</p>
         </v-container>
     </v-container>
 </template>
