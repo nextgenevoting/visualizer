77357e0416c61f9d1a9a14f9723ae2b5a97d2c26
Yannick Denzer <denzy1@bfh.ch>
2017-10-29 20:39:38 +0100

    Add more translations

--- frontend/src/pages/ElectionsPage.vue
+++ frontend/src/pages/ElectionsPage.vue
@@ -1,53 +1,59 @@
 <template>
-    <v-container>
-        <h2>Available election events</h2>
-        <p>Please choose an election:</p>
-        <v-list two-line subheader>
-            <v-list-tile v-for="item in getElections" v-bind:key="item.title" avatar
-                         :to="{ name: 'electionoverview', params: {id: item.id }}">
-                <v-list-tile-avatar>
-                    <v-icon v-bind:class="[item.iconClass]">{{ item.icon }}</v-icon>
-                </v-list-tile-avatar>
-                <v-list-tile-content>
-                    <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-                    <v-list-tile-sub-title>{{ item.subtitle }}</v-list-tile-sub-title>
-                </v-list-tile-content>
-                <v-list-tile-action>
-                    <v-btn icon ripple>
-                        <v-icon class="grey--text text--lighten-1">info</v-icon>
-                    </v-btn>
-                </v-list-tile-action>
-            </v-list-tile>
-            </v-subheader>
-        </v-list>
-        <v-btn light to="newElection">
-            Create new
-        </v-btn>
+  <v-container>
+    <h2 v-t="'title'"></h2>
+    <p v-t="'choose'"></p>
 
-    </v-container>
+    <v-list two-line subheader>
+      <v-list-tile v-for="item in getElections" v-bind:key="item.title" avatar
+          :to="{ name: 'electionoverview', params: {id: item.id }}">
+        <v-list-tile-avatar>
+          <v-icon v-bind:class="[item.iconClass]">{{ item.icon }}</v-icon>
+        </v-list-tile-avatar>
+        <v-list-tile-content>
+          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          <v-list-tile-sub-title>{{ item.subtitle }}</v-list-tile-sub-title>
+        </v-list-tile-content>
+        <v-list-tile-action>
+          <v-btn icon ripple>
+            <v-icon class="grey--text text--lighten-1">info</v-icon>
+          </v-btn>
+        </v-list-tile-action>
+      </v-list-tile>
+    </v-list>
+    <v-btn to="newElection" v-t="'create'" />
+  </v-container>
 </template>
 
+<i18n>
+en:
+  title: Available election events
+  choose: Please choose an election event.
+  create: Create new
+de:
+  title: Verfügbare Wahlereignisse
+  choose: Bitte wählen Sie ein Wahlereignis aus.
+  create: Neue erstellen
+</i18n>
+
 <script>
-    export default {
-        computed: {
-            getElections: function () {
-                var elections = [];
-                this.$store.state.Election.elections.forEach((el) => {
-                    var election = {
-                        id: el._id.$oid,
-                        icon: 'assignment',
-                        iconClass: 'blue white--text',
-                        title: el.title,
-                        subtitle: 'Jan 20, 2018'
-                    };
-                    elections.push(election);
-                });
+export default {
+  computed: {
+    getElections: function () {
+      var elections = []
 
-                return elections;
-            }
-        },
+      this.$store.state.Election.elections.forEach((el) => {
+        elections.push({
+          id: el._id.$oid,
+          icon: 'assignment',
+          iconClass: 'blue white--text',
+          title: el.title,
+          subtitle: 'Jan 20, 2018'
+        })
+      })
 
-        mounted() {
-        },
-    };
+      return elections
+    }
+  },
+  mounted () { }
+}
 </script>
--- frontend/src/pages/election/ElectionOverviewPage.vue
+++ frontend/src/pages/election/ElectionOverviewPage.vue
@@ -1,48 +1,46 @@
 <template>
-    <v-container>
+  <v-container>
+    <div class="layout row wrap">
+      <div class="contentHeader">
+        <i class="mdi icon mdi-view-dashboard"></i>
+      </div>
+      <h3 class="my-3">Overview</h3>
+    </div>
 
-        <div class="layout row wrap">
-            <div class="contentHeader">
-                <i class="mdi icon mdi-view-dashboard"></i>
-            </div>
-            <h3 class="my-3">Overview</h3>
-        </div>
-
-        ID: <b>{{ data.id }}</b>
-        <br>
-        Status: <b>{{ data.status }}</b>
-        <br>
-        <br>
-        <v-btn color="primary" v-on:click="debugVotingSim">Debug VoteSim trigger</v-btn>
-    </v-container>
+    ID: <b>{{ data.id }}</b>
+    <br>
+    Status: <b>{{ data.status }}</b>
+    <br>
+    <br>
+    <v-btn color="primary" v-on:click="debugVotingSim">Debug VoteSim trigger</v-btn>
+  </v-container>
 </template>
 
 <script>
 export default {
   computed: {
-    data() {
+    data () {
       return {
         id: this.$store.state.Election.electionID,
-        status: this.$store.getters.getStatusText,
-      };
+        status: this.$store.getters.getStatusText
+      }
     },
   },
-  created() {
-    this.$socket.emit('join', {election: this.$route.params['id'] });
+  created () {
+    this.$socket.emit('join', { election: this.$route.params['id'] })
   },
   methods: {
-      debugVotingSim: function (event) {
-          this.$http.post('debugVotingSim', {
-                  'election': this.$route.params["id"],
-              }
-          ).then(response => {
-              response.json().then((data) => {
-                  // success callback
-              });
-          }, response => {
-              // error callback
-          });
-      },
+    debugVotingSim: function (event) {
+      this.$http.post('debugVotingSim', {
+        'election': this.$route.params['id']
+      }).then(response => {
+          response.json().then((data) => {
+              // success callback
+          })
+      }, response => {
+          // error callback
+      })
+    }
   }
-};
+}
 </script>
