99a2b35a70c945549f972bedcd4da2faef07e10b
Yannick Denzer <denzy1@bfh.ch>
2017-11-03 10:02:42 +0100

    add eslint configuration and dependencies

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,44 +1,44 @@
-import Vue                   from 'vue'
-import Vuex                  from 'vuex'
-import Vuetify               from 'vuetify'
-import VueRouter             from 'vue-router'
-import VueI18n               from 'vue-i18n'
-import VueI18nDirectives     from 'vue-i18n-directives'
-import VueResource           from 'vue-resource'
-import VueSocketio           from 'vue-socket.io'
-import NProgress             from 'nprogress/nprogress.js'
-import { store }             from './store/store.js'
-import App                   from './App.vue'
-import HomePage              from './pages/HomePage.vue'
-import AboutPage             from './pages/AboutPage.vue'
-import ElectionsPage         from './pages/ElectionsPage.vue'
-import LoginPage             from './pages/auth/LoginPage.vue'
-import NewElectionPage       from './pages/NewElectionPage.vue'
-import VoterPage             from './pages/election/VoterPage.vue'
-import PrintingAuthPage      from './pages/election/PrintingAuthPage.vue'
-import ElectionOverviewPage  from './pages/election/ElectionOverviewPage.vue'
-import ElectionAdminPage     from './pages/election/ElectionAdminPage.vue'
-import BulletinBoardPage     from './pages/election/BulletinBoardPage.vue'
+import Vue from 'vue'
+import Vuex from 'vuex'
+import Vuetify from 'vuetify'
+import VueRouter from 'vue-router'
+import VueI18n from 'vue-i18n'
+import VueI18nDirectives from 'vue-i18n-directives'
+import VueResource from 'vue-resource'
+import VueSocketio from 'vue-socket.io'
+import NProgress from 'nprogress/nprogress.js'
+import { store } from './store/store.js'
+import App from './App.vue'
+import HomePage from './pages/HomePage.vue'
+import AboutPage from './pages/AboutPage.vue'
+import ElectionsPage from './pages/ElectionsPage.vue'
+import LoginPage from './pages/auth/LoginPage.vue'
+import NewElectionPage from './pages/NewElectionPage.vue'
+import VoterPage from './pages/election/VoterPage.vue'
+import PrintingAuthPage from './pages/election/PrintingAuthPage.vue'
+import ElectionOverviewPage from './pages/election/ElectionOverviewPage.vue'
+import ElectionAdminPage from './pages/election/ElectionAdminPage.vue'
+import BulletinBoardPage from './pages/election/BulletinBoardPage.vue'
 import ElectionAuthorityPage from './pages/election/ElectionAuthorityPage.vue'
-import SelectVoterDialog     from './utils/SelectVoterDialog.vue'
-import LoadingOverlay        from './utils/LoadingOverlay.vue'
-import BigIntLabel           from './utils/BigIntLabel.vue'
-import DataCard              from './utils/DataCard.vue'
-import ContentTitle          from './utils/ContentTitle.vue'
-import Toasted               from 'vue-toasted'
+import SelectVoterDialog from './utils/SelectVoterDialog.vue'
+import LoadingOverlay from './utils/LoadingOverlay.vue'
+import BigIntLabel from './utils/BigIntLabel.vue'
+import DataCard from './utils/DataCard.vue'
+import ContentTitle from './utils/ContentTitle.vue'
+import Toasted from 'vue-toasted'
 
 const routes = [
-  { path: '/home',                           name: 'home',              component: HomePage },
-  { path: '/about',                          name: 'about',             component: AboutPage },
-  { path: '/elections',                      name: 'elections',         component: ElectionsPage },
-  { path: '/newelection',                    name: 'newelection',       component: NewElectionPage },
-  { path: '/election/:id',                   name: 'electionoverview',  component: ElectionOverviewPage },
-  { path: '/election/:id/admin',             name: 'electionadmin',     component: ElectionAdminPage },
-  { path: '/election/:id/printing',          name: 'printingauth',      component: PrintingAuthPage },
-  { path: '/election/:id/voter',             name: 'voter',             component: VoterPage },
-  { path: '/election/:id/bulletinBoard',     name: 'bulletinboard',     component: BulletinBoardPage },
+  { path: '/home', name: 'home', component: HomePage },
+  { path: '/about', name: 'about', component: AboutPage },
+  { path: '/elections', name: 'elections', component: ElectionsPage },
+  { path: '/newelection', name: 'newelection', component: NewElectionPage },
+  { path: '/election/:id', name: 'electionoverview', component: ElectionOverviewPage },
+  { path: '/election/:id/admin', name: 'electionadmin', component: ElectionAdminPage },
+  { path: '/election/:id/printing', name: 'printingauth', component: PrintingAuthPage },
+  { path: '/election/:id/voter', name: 'voter', component: VoterPage },
+  { path: '/election/:id/bulletinBoard', name: 'bulletinboard', component: BulletinBoardPage },
   { path: '/election/:id/electionAuthority', name: 'electionauthority', component: ElectionAuthorityPage },
-  { path: '/login',                          name: '/login',            component: LoginPage },
+  { path: '/login', name: '/login', component: LoginPage },
   { path: '*', redirect: { name: 'home' } }
 ]
 
@@ -54,12 +54,12 @@ Vue.use(Vuex)
 Vue.use(VueResource)
 Vue.use(VueI18n)
 Vue.use(VueI18nDirectives)
-Vue.use(Toasted, {position: 'top-right', duration: '4000'})
+Vue.use(Toasted, { position: 'top-right', duration: '4000' })
 Vue.component('BigIntLabel', BigIntLabel)
 Vue.component('DataCard', DataCard)
 Vue.component('LoadingOverlay', LoadingOverlay)
 Vue.component('ContentTitle', ContentTitle)
-Vue.component('SelectVoterDialog', SelectVoterDialog);
+Vue.component('SelectVoterDialog', SelectVoterDialog)
 
 Vue.url.options.root = process.env.URL_ROOT
 Vue.use(VueSocketio, process.env.SOCKETIO_BASE_URL, store)
