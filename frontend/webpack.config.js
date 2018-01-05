3b6cccbaf3e1d73118d57e49814f04a1cf075f01
Yannick Denzer <denzy1@bfh.ch>
2018-01-05 13:29:59 +0100

    Update to Vuetify 1.0.0-beta.2

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -48,6 +48,10 @@ module.exports = {
         exclude: /node_modules/
       },
       {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
         test: /\.styl$/,
         loader: ['style-loader', 'css-loader', 'stylus-loader']
       },
